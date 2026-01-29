d08f9d7f5da83a9732981668a14d34191339b199
hudp-group <hudp1990@gmail.com>
2026-01-29 23:07:45 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 (function(){
-  const path = location.pathname.split('/').pop() || 'index.html';
+  let path = location.pathname.split('/').pop() || 'index.html';
+  if(path === 'publications.html') path = 'publication.html';
   document.querySelectorAll('.navbar-nav .nav-link').forEach(a=>{
     const href = a.getAttribute('href');
     if(href === path){ a.classList.add('active'); }
